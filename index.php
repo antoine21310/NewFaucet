00d5add2185844af0d4f6add820fe030ad2cb374
Antoine <antoine21310@gmail.com>
2018-06-07 19:00:18 +0200

    login

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
    <script type="text/javascript" src="js/popper.js"></script>
    <script type="text/javascript" src="js/bootstrap.min.js"></script>
    <script type="text/javascript" src="js/particles.js"></script>
+   <script type="text/javascript" src="js/popover.js"></script>
+
 </head>
 <body>
    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -34,14 +36,97 @@
                <a class="nav-link " href="#">Litecoin</a>
             </li>
             <li class="nav-item">
-               <a class="nav-link " href="#">Dashcoincoin</a>
+               <a class="nav-link " href="#">Dashcoin</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" data-toggle="modal" data-target="#exampleModal" href="#">Login/Register</a>
             </li>
          </ul>
       </div>
    </nav>
-   <div class="container-fluid">
-      <div class="row">
-         <div class="col-md-2 col-md-pull-10">
+
+
+
+   <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+     <div class="modal-content">
+      <div class="modal-header">
+       <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+     </button>
+  </div>
+  <div class="modal-body">
+    ...
+ </div>
+ <div class="modal-footer">
+    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+    <button type="button" class="btn btn-primary">Save changes</button>
+ </div>
+</div>
+</div>
+</div>
+
+
+
+<div class="container-fluid">
+   <div class="row">
+
+      <div class="col-md-10 col-md-push-2">
+         <div class="card shadow">
+            <div class="card-header">Claim</div>
+            <div class="card-body">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+            </div>
+
+         </div>
+      </div>
+
+      <div class="col-md-2 col-md-pull-10">
+         <div class="card shadow">
+            <div class="card-header">News</div>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -49,135 +134,82 @@
             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
          </div>
-         <div class="col-md-10 col-md-push-2">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+
+   </div>
+</div>
+</div>
+<div class="container-fluid">
+   <div class="row">
+
+      <div class="col-md">
+         <div class="card shadow animated jackInTheBox">
+            <div class="card-header">Bitcoin</div>
+            <div class="card-body text-center">100€</div>
          </div>
+      </div>
+
+      <div class="col-md">
+         <div class="card shadow animated jackInTheBox">
+            <div class="card-header">Bitcoin</div>
+            <div class="card-body text-center">100€</div>
+         </div>
+      </div>
+
+      <div class="col-md">
+         <div class="card shadow animated jackInTheBox">
+            <div class="card-header">Bitcoin</div>
+            <div class="card-body text-center">100€</div>
+         </div>
+      </div>
+
+      <div class="col-md">
+         <div class="card shadow animated jackInTheBox">
+            <div class="card-header">Bitcoin</div>
+            <div class="card-body text-center">100€</div>
+         </div>
+      </div>
+
+      <div class="col-md">
+         <div class="card shadow animated jackInTheBox">
+            <div class="card-header">Bitcoin</div>
+            <div class="card-body text-center">100€</div>
+         </div>
+      </div>
+
+   </div>
+</div>
+<footer class="container-fluid page-footer ">
+   <div class="row">
+      <div class="col-md-6 mt-md-0 mt-3">
+         <h5 class="text-uppercase">Footer Content</h5>
+         <p>Here you can use rows and columns here to organize your footer content.</p>
+      </div>
+      <hr class="clearfix w-100 d-md-none pb-3">
+      <div class="col-md-6 mb-md-0 mb-3">
+         <h5 class="text-uppercase">Links</h5>
+         <ul class="list-unstyled">
+            <li>
+               <a href="#!">Link 1</a>
+            </li>
+            <li>
+               <a href="#!">Link 2</a>
+            </li>
+            <li>
+               <a href="#!">Link 3</a>
+            </li>
+            <li>
+               <a href="#!">Link 4</a>
+            </li>
+         </ul>
       </div>
    </div>
-   <div class="container-fluid">
-      <div class="row">
-
-         <div class="col-md">
-            <div class="card shadow animated jackInTheBox">
-               <div class="card-header">Bitcoin</div>
-               <div class="card-body">100€</div>
-            </div>
-         </div>
-
-         <div class="col-md">
-            <div class="card shadow animated jackInTheBox">
-               <div class="card-header">Bitcoin</div>
-               <div class="card-body">100€</div>
-            </div>
-         </div>
-
-         <div class="col-md">
-            <div class="card shadow animated jackInTheBox">
-               <div class="card-header">Bitcoin</div>
-               <div class="card-body">100€</div>
-            </div>
-         </div>
-
-         <div class="col-md">
-            <div class="card shadow animated jackInTheBox">
-               <div class="card-header">Bitcoin</div>
-               <div class="card-body">100€</div>
-            </div>
-         </div>
-
-         <div class="col-md">
-            <div class="card shadow animated jackInTheBox">
-               <div class="card-header">Bitcoin</div>
-               <div class="card-body">100€</div>
-            </div>
-         </div>
-
+   <div class="row">
+      <div class="footer-copyright container-fluid text-center">© 2018 Copyright:
+         <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a>
       </div>
    </div>
-   <footer class="container-fluid page-footer ">
-      <div class="row">
-         <div class="col-md-6 mt-md-0 mt-3">
-            <h5 class="text-uppercase">Footer Content</h5>
-            <p>Here you can use rows and columns here to organize your footer content.</p>
-         </div>
-         <hr class="clearfix w-100 d-md-none pb-3">
-         <div class="col-md-6 mb-md-0 mb-3">
-            <h5 class="text-uppercase">Links</h5>
-            <ul class="list-unstyled">
-               <li>
-                  <a href="#!">Link 1</a>
-               </li>
-               <li>
-                  <a href="#!">Link 2</a>
-               </li>
-               <li>
-                  <a href="#!">Link 3</a>
-               </li>
-               <li>
-                  <a href="#!">Link 4</a>
-               </li>
-            </ul>
-         </div>
-      </div>
-      <div class="row">
-         <div class="footer-copyright container-fluid text-center">© 2018 Copyright:
-            <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a>
-         </div>
-      </div>
-   </footer>
+</footer>
 </body>
 </html>
 
