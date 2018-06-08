9f1123782bcfaf77d6cbad80d6a1447a813ebf46
Antoine <antoine21310@gmail.com>
2018-06-08 19:24:16 +0200

    ad

---
+++ account.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Faucet</title>
+ <meta name="viewport" content="width=device-width" />
+ <meta charset="utf-8">
+ <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+ <link rel="stylesheet" type="text/css" href="css/style.css">
+
+ <script type="text/javascript" src="js/jquery.js"></script>
+ <script type="text/javascript" src="js/popper.js"></script>
+ <script type="text/javascript" src="js/bootstrap.min.js"></script>
+ <script type="text/javascript" src="js/particles.js"></script>
+ <script type="text/javascript" src="js/popover.js"></script>
+
+</head>
+<body>
+
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark animated fadeIn shadow">
+   <a class="navbar-brand" href="#">Cryptomine</a>
+   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+     <span class="navbar-toggler-icon"></span>
+   </button>
+   <div class="collapse navbar-collapse animated fadeInRight" id="navbarText">
+     <ul class="navbar-nav mr-auto">
+       <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Bitcoin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Dogecoin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Litecoin</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link " href="#">Dashcoin</a>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      <a class="" href="/account.php">My Account</a>
+      <a class="" data-toggle="modal" data-target="#login" href="#">Login / Register</a>
+    </span>
+  </div>
+</nav>
+
+
+
+<div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+ <div class="modal-dialog modal-dialog-centered" role="document">
+  <div class="modal-content">
+   <div class="modal-header">
+    <h5 class="modal-title" id="exampleModalLabel">Login / Register</h5>
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+     <span aria-hidden="true">&times;</span>
+   </button>
+ </div>
+ <div class="modal-body">
+   <form>
+     <div class="form-group">
+       <label for="exampleInputEmail1">Email address</label>
+       <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+     </div>
+     <div class="form-group">
+       <label for="exampleInputPassword1">Password</label>
+       <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+     </div>
+   </div>
+   <button type="submit" class="btn btn-primary">Login / Register</button>
+ </form>
+</div>
+</div>
+</div>
+</div>
+
+
+
+<div class="container-fluid">
+ <div class="row">
+
+  <div class="col-md-9 col-md-push-3">
+   <div class="card shadow animated fadeIn">
+    <div class="card-header">Claim</div>
+    <div class="card-body">
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+     tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+     quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+     consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+     proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+   </div>
+
+ </div>
+</div>
+
+<div class="col-md-3 col-md-pull-9">
+ <div class="card shadow">
+  <div class="card-header">News</div>
+  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</div>
+</div>
+
+</div>
+</div>
+</div>
+<div class="container-fluid">
+ <div class="row">
+
+  <div class="col-md">
+   <div class="card shadow animated jackInTheBox">
+    <div class="card-header">Bitcoin</div>
+    <div class="card-body text-center">100€</div>
+  </div>
+</div>
+
+<div class="col-md">
+ <div class="card shadow animated jackInTheBox">
+  <div class="card-header">Bitcoin</div>
+  <div class="card-body text-center">100€</div>
+</div>
+</div>
+
+<div class="col-md">
+ <div class="card shadow animated jackInTheBox">
+  <div class="card-header">Bitcoin</div>
+  <div class="card-body text-center">100€</div>
+</div>
+</div>
+
+<div class="col-md">
+ <div class="card shadow animated jackInTheBox">
+  <div class="card-header">Bitcoin</div>
+  <div class="card-body text-center">100€</div>
+</div>
+</div>
+
+<div class="col-md">
+ <div class="card shadow animated jackInTheBox">
+  <div class="card-header">Bitcoin</div>
+  <div class="card-body text-center">100€</div>
+</div>
+</div>
+
+</div>
+</div>
+<footer class="container-fluid page-footer ">
+ <div class="row">
+  <div class="col-md-6 mt-md-0 mt-3">
+   <h5 class="text-uppercase">Footer Content</h5>
+   <p>Here you can use rows and columns here to organize your footer content.</p>
+ </div>
+ <hr class="clearfix w-100 d-md-none pb-3">
+ <div class="col-md-6 mb-md-0 mb-3">
+   <h5 class="text-uppercase">Links</h5>
+   <ul class="list-unstyled">
+    <li>
+     <a href="#!">Link 1</a>
+   </li>
+   <li>
+     <a href="#!">Link 2</a>
+   </li>
+   <li>
+     <a href="#!">Link 3</a>
+   </li>
+   <li>
+     <a href="#!">Link 4</a>
+   </li>
+ </ul>
+</div>
+</div>
+<div class="row">
+  <div class="footer-copyright container-fluid text-center">© 2018 Copyright:
+   <a href="https://mdbootstrap.com/bootstrap-tutorial/"> MDBootstrap.com</a>
+ </div>
+</div>
+</footer>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,88 +1,80 @@
+
+
 <!DOCTYPE html>
 <html>
 <head>
    <title>Faucet</title>
-   <meta name="viewport" content="width=device-width" />
+   <meta name="viewport" content="width=device-width, user-scalable=no">
    <meta charset="utf-8">
-   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/bootstrap.min.css">
    <link rel="stylesheet" type="text/css" href="css/animate.min.css">
+   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
    <link rel="stylesheet" type="text/css" href="css/style.css">
-
    <script type="text/javascript" src="js/jquery.js"></script>
    <script type="text/javascript" src="js/popper.js"></script>
    <script type="text/javascript" src="js/bootstrap.min.js"></script>
    <script type="text/javascript" src="js/particles.js"></script>
    <script type="text/javascript" src="js/popover.js"></script>
-
 </head>
 <body>
-   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a class="navbar-brand" href="#">Navbar</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark animated fadeIn shadow">
+      <a class="navbar-brand" href="#">Cryptomine</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
          <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-         <ul class="navbar-nav animated fadeInRight">
+      <div class="collapse navbar-collapse animated fadeInRight" id="navbarText">
+         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-               <a class="nav-link" href="#">Bitcoin</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="#">Dogecoin</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link " href="#">Litecoin</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link " href="#">Dashcoin</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" data-toggle="modal" data-target="#login" href="#">Login / Register</a>
+               <a class="nav-link" href="#">Claim</a>
             </li>
          </ul>
+         <span class="navbar-text">
+            <i class="fas fa-user"></i><a class="" href="/account.php">My Account</a>
+            <a class="" data-toggle="modal" data-target="#login" href="#">Login / Register</a>
+         </span>
       </div>
    </nav>
-
-
-
    <div class="modal fade" id="login" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-     <div class="modal-dialog modal-dialog-centered" role="document">
-       <div class="modal-content">
-         <div class="modal-header">
-           <h5 class="modal-title" id="exampleModalLabel">Login / Register</h5>
-           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-             <span aria-hidden="true">&times;</span>
-          </button>
-       </div>
-       <div class="modal-body">
-         <form>
-          <div class="form-group">
-           <label for="exampleInputEmail1">Email address</label>
-           <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-           <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-           <label for="exampleInputPassword1">Password</label>
-           <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-        </div>
-        </div>
-        <button type="submit" class="btn btn-primary">Login / Register</button>
-     </form>
-  </div>
+      <div class="modal-dialog modal-dialog-centered" role="document">
+         <div class="modal-content">
+            <div class="modal-header">
+               <h5 class="modal-title" id="exampleModalLabel">Login / Register</h5>
+               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+               </button>
+            </div>
+            <div class="modal-body">
+               <form>
+                  <div class="form-group">
+                     <label for="exampleInputEmail1">Email address</label>
+                     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                     <label for="exampleInputPassword1">Password</label>
+                     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+               </div>
+               <button type="submit" class="btn btn-primary">Login / Register</button>
+            </form>
+         </div>
+      </div>
+   </div>
 </div>
-</div>
-</div>
-
-
-
 <div class="container-fluid">
    <div class="row">
-
-      <div class="col-md-9 col-md-push-3">
-         <div class="card shadow">
-            <div class="card-header">Claim</div>
+      <div class="col-md-9">
+         <div class="card card-top shadow animated fadeInLeftBig">
+            <div class="card-body">
+               <center>
+                  <pre><div style="height: 90px; width: 728px; background-color: red;"></div></pre>
+               </center>
+            </div>
+         </div>
+         <div class="card shadow animated fadeInLeftBig">
+            <div class="card-header">News</div>
             <div class="card-body">
                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
@@ -108,33 +100,57 @@
                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
             </div>
-
+         </div>
+         <div class="card shadow animated fadeInLeftBig">
+            <div class="card-header">What is Bitcoin</div>
+            <div class="card-body">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
          </div>
       </div>
-
-      <div class="col-md-3 col-md-pull-9">
-         <div class="card shadow">
-            <div class="card-header">News</div>
+      <div class="col-md-3">
+         <div class="card card-top shadow animated fadeInRightBig">
+            <div class="card-header">Last claims</div>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+         </div>
+         <div class="card shadow animated fadeInRightBig">
+            <div class="card-body">
+               <center>
+                  <pre><div style="height: 100px; width: 300px; background-color: red;"></div></pre>
+               </center>
+            </div>
+         </div>
+         <div class="card shadow animated fadeInRightBig">
+            <div class="card-header">Last withdrawals</div>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -143,48 +159,40 @@
             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
          </div>
       </div>
-
    </div>
-</div>
 </div>
 <div class="container-fluid">
    <div class="row">
-
       <div class="col-md">
-         <div class="card shadow animated jackInTheBox">
+         <div class="card shadow animated swing">
             <div class="card-header">Bitcoin</div>
             <div class="card-body text-center">100€</div>
          </div>
       </div>
-
       <div class="col-md">
-         <div class="card shadow animated jackInTheBox">
+         <div class="card shadow animated swing">
             <div class="card-header">Bitcoin</div>
             <div class="card-body text-center">100€</div>
          </div>
       </div>
-
       <div class="col-md">
-         <div class="card shadow animated jackInTheBox">
+         <div class="card shadow animated swing">
             <div class="card-header">Bitcoin</div>
             <div class="card-body text-center">100€</div>
          </div>
       </div>
-
       <div class="col-md">
-         <div class="card shadow animated jackInTheBox">
+         <div class="card shadow animated swing">
             <div class="card-header">Bitcoin</div>
             <div class="card-body text-center">100€</div>
          </div>
       </div>
-
       <div class="col-md">
-         <div class="card shadow animated jackInTheBox">
+         <div class="card shadow animated swing">
             <div class="card-header">Bitcoin</div>
             <div class="card-body text-center">100€</div>
          </div>
       </div>
-
    </div>
 </div>
 <footer class="container-fluid page-footer ">
